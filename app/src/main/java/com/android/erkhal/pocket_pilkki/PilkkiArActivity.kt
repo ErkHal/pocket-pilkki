e662263c2ad25399c7c4de71bc1bf5db2d2da9c8
ErkHal <erkkihalinen@gmail.com>
2018-09-20 14:02:14 +0300

    #1: Initial shell project

---
+++ app/src/main/java/com/android/erkhal/pocket_pilkki/PilkkiArActivity.kt
@@ -0,0 +1,12 @@
+package com.android.erkhal.pocket_pilkki
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PilkkiArActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilkki_ar)
+    }
+}
