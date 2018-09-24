53667684cd1873622daf6fad9211c09a53896db3
ErkHal <erkkihalinen@gmail.com>
2018-09-24 14:02:33 +0300

    #5: Catching mechanism: Added controller for the g-force calculations to detect when the player jerks the fishing rod

---
+++ app/src/main/java/com/android/erkhal/pocket_pilkki/AccelerometerController.kt
@@ -0,0 +1,69 @@
+package com.android.erkhal.pocket_pilkki
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Vibrator
+import android.util.Log
+import kotlin.math.sqrt
+
+//Constants
+const val SHAKE_THRESHOLD_ACTIVITY = 5f
+const val SHAKE_SLOP_TIME_MS = 500
+
+class AccelerometerController(ctx: Context): SensorEventListener {
+
+    interface AcceleroMeterControllerListener {
+        fun onDeviceJerked()
+    }
+
+    private var context = ctx
+    private var sensorManager: SensorManager
+
+    private var x = 0f
+    private var y = 0f
+    private var z = 0f
+
+    private var mShakeTimestamp: Long = 0
+
+    init {
+
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("Accelerometer", "Sensor accuracy changed to $accuracy")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val vibratorService = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        if (event != null) {
+            x = event.values[0]
+            y = event.values[1]
+            z = event.values[2]
+
+
+            val gX = x / SensorManager.GRAVITY_EARTH
+            val gY = y / SensorManager.GRAVITY_EARTH
+            val gZ = z / SensorManager.GRAVITY_EARTH
+
+            //gForce is approx 1 when no movement
+            val gForce = sqrt(gX * gX + gY * gY + gZ * gZ)
+
+            if (gForce > SHAKE_THRESHOLD_ACTIVITY) {
+                    notifyActivity()
+            }
+        }
+    }
+    private fun notifyActivity() {
+        val listener = context as AccelerometerController.AcceleroMeterControllerListener
+        listener.onDeviceJerked()
+    }
+}
--- app/src/main/java/com/android/erkhal/pocket_pilkki/PilkkiArActivity.kt
+++ app/src/main/java/com/android/erkhal/pocket_pilkki/PilkkiArActivity.kt
@@ -1,40 +1,26 @@
 package com.android.erkhal.pocket_pilkki
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Vibrator
 import android.util.Log
+import com.google.ar.core.HitResult
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_pilkki_ar.*
-import kotlin.math.sqrt
 
-class PilkkiArActivity : AppCompatActivity(), SensorEventListener {
+class PilkkiArActivity : AppCompatActivity(), AccelerometerController.AcceleroMeterControllerListener {
 
     //Renderables for the AR scene
     private lateinit var fishingPondRenderable: ModelRenderable
 
+    // Fragments
     private lateinit var arFragment: ArFragment
 
-    lateinit var sensorManager: SensorManager
-
-    var x = 0f
-    var y = 0f
-    var z = 0f
-
-    private val SHAKE_THRESHOLD_GRAVITY = 5f
-    private val SHAKE_SLOP_TIME_MS = 500
-
-    private var mShakeTimestamp: Long = 0
+    //Controllers
+    private lateinit var accelerometerController: AccelerometerController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,44 +28,42 @@ class PilkkiArActivity : AppCompatActivity(), SensorEventListener {
 
         arFragment = ar_fragment as ArFragment
 
+        accelerometerController = AccelerometerController(this)
+
         setupFishingPondRenderable()
 
         //calls the top bar functionality
         fishingBarController()
 
-        //
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        sensorManager.registerListener(
-                this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_GAME)
-
         arFragment.setOnTapArPlaneListener(
-                BaseArFragment.OnTapArPlaneListener { hitResult, _, _ ->
-
-                    if (fishingPondRenderable == null) {
-                        return@OnTapArPlaneListener
-                    }
-
-                    val anchor = hitResult!!.createAnchor()
-                    val anchorNode = AnchorNode(anchor)
-                    anchorNode.setParent(arFragment.arSceneView.scene)
-                    val viewNode = TransformableNode(arFragment.transformationSystem)
-                    viewNode.setParent(anchorNode)
-
-                    disableViewNodeController(viewNode)
-
-                    viewNode.renderable = fishingPondRenderable
-                    viewNode.select()
-
+                { hitResult, _, _ ->
+                    spawnFishingPond(hitResult)
                 })
+    }
+
+    override fun onDeviceJerked() {
+
     }
 
     private fun disableViewNodeController(viewNode: TransformableNode) {
         viewNode.rotationController.isEnabled = false
         viewNode.scaleController.isEnabled = false
         viewNode.translationController.isEnabled = false
+    }
+
+    private fun spawnFishingPond(hitResult: HitResult) {
+
+        val anchor = hitResult!!.createAnchor()
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.setParent(arFragment.arSceneView.scene)
+        val viewNode = TransformableNode(arFragment.transformationSystem)
+        viewNode.setParent(anchorNode)
+
+        disableViewNodeController(viewNode)
+
+        viewNode.renderable = fishingPondRenderable
+        viewNode.select()
+
     }
 
     private fun setupFishingPondRenderable() {
@@ -96,43 +80,5 @@ class PilkkiArActivity : AppCompatActivity(), SensorEventListener {
     private fun fishingBarController() {
         // testing the progressbar
         fishingBar.setProgress(50)
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-
-        val vibratorService = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-
-        if (event != null) {
-            x = event!!.values[0]
-            y = event.values[1]
-            z = event.values[2]
-
-
-            var gX = x / SensorManager.GRAVITY_EARTH
-            var gY = y / SensorManager.GRAVITY_EARTH
-            var gZ = z / SensorManager.GRAVITY_EARTH
-
-            //gForce is approx 1 when no movement
-            var gForce = sqrt(gX * gX + gY * gY + gZ * gZ)
-
-            tvGForce.text = "${gForce}"
-
-            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
-                var curTime = System.currentTimeMillis().toFloat()
-
-                //ignore shake events too close to each other (500ms)
-                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > curTime) {
-                    return
-                } else {
-                    mShakeTimestamp = curTime.toLong()
-                    vibratorService.vibrate(100)
-
-                }
-            }
-        }
     }
 }
